c1f060fa52a28e418907242991efda82cc0917ba
sunjin110 <sunjin110@gmail.com>
2023-03-04 23:44:45 +0900

    fmt

--- src/utils/linked_str_list/linked_str_list.c
+++ src/utils/linked_str_list/linked_str_list.c
@@ -1,77 +1,78 @@
-#include <memory.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "linked_str_list.h"
 
-extern linked_str_list* new_linked_str_list() {
-    linked_str_list *list = malloc(sizeof(linked_str_list));
-    list->size = 0;
-    list->first_ptr = NULL;
-    list->last_ptr = NULL;
-    return list;
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern linked_str_list *new_linked_str_list() {
+  linked_str_list *list = malloc(sizeof(linked_str_list));
+  list->size = 0;
+  list->first_ptr = NULL;
+  list->last_ptr = NULL;
+  return list;
 }
 
 extern void append_linked_str_list(linked_str_list *list, char *value) {
-    list->size++;
+  list->size++;
 
-    linked_str_element *new_element = malloc(sizeof(linked_str_element));
-    new_element->value = malloc(strlen(value));
-    strcpy(new_element->value, value);
-    new_element->next_ptr = NULL;
-    if (list->first_ptr == NULL) {
-        list->first_ptr = new_element;
-        list->last_ptr = new_element;
-        return;
-    }
-    list->last_ptr->next_ptr = new_element;
+  linked_str_element *new_element = malloc(sizeof(linked_str_element));
+  new_element->value = malloc(strlen(value));
+  strcpy(new_element->value, value);
+  new_element->next_ptr = NULL;
+  if (list->first_ptr == NULL) {
+    list->first_ptr = new_element;
     list->last_ptr = new_element;
+    return;
+  }
+  list->last_ptr->next_ptr = new_element;
+  list->last_ptr = new_element;
 }
 
 extern void free_linked_str_list(linked_str_list *list) {
-    linked_str_element *current_element = list->first_ptr;
-    for (;;) {
-        if (current_element == NULL) {
-            break;
-        }
-        linked_str_element *tmp_ptr = current_element->next_ptr;
-        free(current_element->value);
-        current_element->value = NULL;
-        free(current_element);
-        current_element = tmp_ptr;
+  linked_str_element *current_element = list->first_ptr;
+  for (;;) {
+    if (current_element == NULL) {
+      break;
     }
-    list->first_ptr = NULL;
-    list->last_ptr = NULL;
-    list->size = 0;
-    free(list);
+    linked_str_element *tmp_ptr = current_element->next_ptr;
+    free(current_element->value);
+    current_element->value = NULL;
+    free(current_element);
+    current_element = tmp_ptr;
+  }
+  list->first_ptr = NULL;
+  list->last_ptr = NULL;
+  list->size = 0;
+  free(list);
 }
 
 extern bool find_in_linked_str_list(linked_str_list *list, char *target_value) {
-    linked_str_element *current_element = list->first_ptr;
-    for (;;) {
-        if (current_element == NULL) {
-            return false;
-        }
-        if (strcmp(current_element->value, target_value) == 0) {
-            return true;
-        }
-        current_element = current_element->next_ptr;
+  linked_str_element *current_element = list->first_ptr;
+  for (;;) {
+    if (current_element == NULL) {
+      return false;
     }
-    return false;
+    if (strcmp(current_element->value, target_value) == 0) {
+      return true;
+    }
+    current_element = current_element->next_ptr;
+  }
+  return false;
 }
 
 extern void print_linked_str_list(linked_str_list *list) {
-    linked_str_element *current_element = list->first_ptr;
-    printf("[");
-    for (;;) {
-        if (current_element == NULL) {
-            return;
-        }
-        if (current_element->next_ptr == NULL) {
-            printf("%s]\n", current_element->value);
-            return;
-        }
-        printf("%s,", current_element->value);
-        current_element = current_element->next_ptr;
+  linked_str_element *current_element = list->first_ptr;
+  printf("[");
+  for (;;) {
+    if (current_element == NULL) {
+      return;
     }
+    if (current_element->next_ptr == NULL) {
+      printf("%s]\n", current_element->value);
+      return;
+    }
+    printf("%s,", current_element->value);
+    current_element = current_element->next_ptr;
+  }
 }
--- src/utils/linked_str_list/linked_str_list.h
+++ src/utils/linked_str_list/linked_str_list.h
@@ -1,17 +1,17 @@
 #include <stdbool.h>
 
 typedef struct _linked_str_list {
-    struct _linked_str_element *first_ptr;
-    struct _linked_str_element *last_ptr;
-    size_t size;
+  struct _linked_str_element *first_ptr;
+  struct _linked_str_element *last_ptr;
+  size_t size;
 } linked_str_list;
 
 typedef struct _linked_str_element {
-    struct _linked_str_element *next_ptr;
-    char *value;
+  struct _linked_str_element *next_ptr;
+  char *value;
 } linked_str_element;
 
-extern linked_str_list* new_linked_str_list(void); 
+extern linked_str_list *new_linked_str_list(void);
 
 extern void append_linked_str_list(linked_str_list *list, char *value);
 extern bool find_in_linked_str_list(linked_str_list *list, char *target_value);
