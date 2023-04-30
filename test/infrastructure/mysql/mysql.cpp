1ab2d2b27cb680badf0041c9762c2970a8ec68f2
sunjin110 <sunjin110@gmail.com>
2023-04-30 17:27:57 +0900

    dockerとlocalのtest差分を無くす

--- test/infrastructure/mysql/mysql.cpp
+++ test/infrastructure/mysql/mysql.cpp
@@ -1,16 +1,58 @@
 
 #include "mysql.h"
 
-#include <stdlib.h>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <string.h>
+
+using std::getenv;
+using std::string;
+using std::cout; 
+using std::endl;
+
+static int convert_port(char *port_str) {
+  if (port_str == NULL) {
+    return 13306;
+  }
+  auto port = atoi(port_str);
+  if (port <= 0) {
+    return 13306;
+  }
+  return port;
+}
 
 extern mysql_config *test_mysql_config() {
+
+  cout << "======== test mysql config =========" << endl;
+  auto *database = getenv("C_SERVER_TEST_MYSQL_DATABASE");
+  if (database == NULL) {
+    database = "c_server";
+  }
+
+  auto user = getenv("C_SERVER_TEST_MYSQL_USER");
+  if (user == NULL) {
+    user = "c_server_user";
+  }
+
+  auto password = getenv("C_SERVER_TEST_MYSQL_PASSWORD");
+  if (password == NULL) {
+    password = "passw0rd";
+  }
+
+  auto host = getenv("C_SERVER_TEST_MYSQL_HOST");
+  if (host == NULL) {
+    host = "127.0.0.1";
+  }
+
+  auto port_str = getenv("C_SERVER_TEST_MYSQL_PORT");
+  auto port = convert_port(port_str);
+
   mysql_config *cfg = (mysql_config *)malloc(sizeof(mysql_config));
-  cfg->database = "c_server";
-  cfg->user = "c_server_user";
-  cfg->pass = "passw0rd";
-  // cfg->host = "127.0.0.1";
-  cfg->host = "mariadb";
-  //   cfg->port = 13306;
-  cfg->port = 3306;
+  cfg->database = strdup(database);
+  cfg->user = strdup(user);
+  cfg->pass = strdup(password);
+  cfg->host = strdup(host);
+  cfg->port = port;
   return cfg;
 }
